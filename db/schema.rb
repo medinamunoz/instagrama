2eae0d6525092feba6a94ae32c08b81ccccaa269
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-06-03 17:30:25 -0400

    scaffold for photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603025331) do
+ActiveRecord::Schema.define(version: 20170603211353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "content"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
